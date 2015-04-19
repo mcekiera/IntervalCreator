e9758e40fddbed48c83d9bdebb5e8c80a72cf646
Pacin <marcin.cekiera@gmail.com>
2015-04-19 20:38:27 +0200

    Change Interval internal structure - one arraylist of arrays

--- src/Interval/Interval.java
+++ src/Interval/Interval.java
@@ -3,25 +3,21 @@ package Interval;
 import java.util.ArrayList;
 
 public class Interval {
-    private ArrayList<String> schedule;
+    protected ArrayList<String[]> schedule;
     private String name;
-    private String message;
+
 
     public Interval(String name){
         this.name = name;
-        schedule = new ArrayList<String>();
+        schedule = new ArrayList<String[]>();
+    }
+    public void addToSchedule(String time, String message, String alarm){
+        String[] position = {time, message, alarm};
+        schedule.add(position);
+
     }
 
-    public void registerTime(String time){
-        if(!checkStringFormat(time)) System.out.println("Wrong input format");
-        this.schedule.add(time);
-    }
-    public String extractTime(int index){
-        return schedule.get(index);
-    }
-    static boolean checkStringFormat(String toCheck){
-        return (toCheck.matches("^(\\d{2}:\\d{2}:\\d{2}$)"));
-
-        //TODO: ta metoda powinna sprawdzać a później zmianiać format;
+    public String toString(){
+        return this.name;
     }
 }
--- src/Interval/IntervalTest.java
+++ src/Interval/IntervalTest.java
@@ -5,33 +5,23 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertTrue;
 
 public class IntervalTest {
     Interval interval;
 
-@Before
-public void before() throws Exception {
-    interval = new Interval("int");
-} 
+    @Before
+    public void before() throws Exception {
+        interval = new Interval("int");
+    }
 
-@After
-public void after() throws Exception { 
-} 
+    @After
+    public void after() throws Exception {
+    }
 
-@Test
-public void testRegisterAndExtractTime() throws Exception {
-    String time = "00:45:23";
-    interval.registerTime(time);
-    assertEquals(time,interval.extractTime(0));
-} 
-
-@Test
-public void testCheckStringFormat() throws Exception {
-    assertTrue(Interval.checkStringFormat("00:00:00"));
-    assertFalse(Interval.checkStringFormat("000000"));
-} 
+    @Test
+    public void testToString(){
+        assertEquals("int",interval.toString());
+    }
 
 
 } 
